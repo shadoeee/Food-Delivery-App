a60f963bc23a6969c018ef3bc7981917074145c7
shadoeee <balaganesh541@gmail.com>
2024-04-30 13:11:42 +0530

    s

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -1,33 +1,36 @@
 import React, { useContext, useEffect } from 'react';
-import axios from 'axios'; // Import axios module
+import axios from 'axios';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 const Verify = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const success = searchParams.get("success");
     const orderId = searchParams.get("orderId");
     const { url } = useContext(StoreContext);
     const navigate = useNavigate();
 
-    const verifyPayment = async () => {
-        try {
-            const response = await axios.post(`${url}/api/order/verify`, { success, orderId });
-            if (response.data.success) {
-                navigate("/myorders");
-            } else {
+    useEffect(() => {
+        const verifyPayment = async () => {
+            try {
+                const response = await axios.post(`${url}/api/order/verify`, { success, orderId });
+                if (response.data.success) {
+                    navigate("/myorders");
+                } else {
+                    navigate("/");
+                }
+            } catch (error) {
+                console.error("Error verifying payment:", error);
                 navigate("/");
             }
-        } catch (error) {
-            console.error("Error verifying payment:", error);
-            // Handle error appropriately
+        };
+
+        if (success && orderId) {
+            verifyPayment();
+        } else {
             navigate("/");
         }
-    };
-
-    useEffect(() => {
-        verifyPayment();
-    }, []); // Empty dependency array to run the effect only once on mount
+    }, [success, orderId, navigate, url]);
 
     return (
         <div className='verify'>
